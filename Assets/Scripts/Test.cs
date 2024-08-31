72bdaf906fe77d169e341a3d045ffb71614112a6
Maksim Kryvanos <maksim.kryvanosint@unity3d.com>
2024-08-31 18:04:49 +0200

    Added sprites

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    // Start is called before the first frame update
+    private void Start() { }
+
+    // Update is called once per frame
+    private void Update() { }
+
+    #endregion
+}
